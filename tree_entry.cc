da7e5b20cc89b9d55badb003995b77cae29d9623
Cole <cnemec@student.austintrinity.org>
2023-05-30 21:09:00 -0700

    Implemented tree_entry
    Renamed entry to tree_entry to avoid conflict with C++ builtin stuff

---
+++ tree_entry.cc
@@ -0,0 +1,43 @@
+
+#include "tree_entry.h"
+
+
+template<class KeyType, class ItemType>
+void tree_entry<KeyType, ItemType>::setKey(const KeyType &sk) {
+	searchKey = sk;
+}
+
+
+
+template<class KeyType, class ItemType>
+tree_entry<KeyType, ItemType>::tree_entry() {} // default
+
+template<class KeyType, class ItemType>
+tree_entry<KeyType, ItemType>::~tree_entry() {} //default
+
+template<class KeyType, class ItemType>
+ItemType tree_entry<KeyType, ItemType>::getItem() const {
+	return item;
+}
+
+template<class KeyType, class ItemType>
+KeyType tree_entry<KeyType, ItemType>::getKey() const {
+	return searchKey;
+}
+
+template<class KeyType, class ItemType>
+void tree_entry<KeyType, ItemType>::setItem(const ItemType &newEntry) {
+	item = newEntry;
+}
+
+
+
+template <class KeyType, class ItemType>
+bool tree_entry<KeyType, ItemType>::operator==(const entry<KeyType, ItemType> &rhs) const {
+	return(searchKey == rhs.getKey());
+}
+
+template <class KeyType, class ItemType>
+bool tree_entry<KeyType, ItemType>::operator>(const entry<KeyType, ItemType> &rhs) const { // todo check
+	return(searchKey > rhs.getKey());
+}
---
+++ tree_entry.h
@@ -0,0 +1,29 @@
+
+#ifndef SOURCE_TREE_ENTRY_H
+#define SOURCE_TREE_ENTRY_H
+
+class SearchKey{ // TODO should this just be a part of tree_entry?
+
+};
+
+template <class KeyType, class ItemType>
+class tree_entry {
+private:
+	ItemType item;
+	KeyType searchKey;
+protected:
+	void setKey(const KeyType& sk); // Protected to prevent calling outside of member functions
+
+public:
+	tree_entry();
+	~tree_entry();
+	ItemType getItem() const;
+	KeyType getKey() const;
+	void setItem(const ItemType& newEntry);
+
+	bool operator==(const tree_entry<KeyType, ItemType>& rhs) const;
+	bool operator>(const tree_entry<KeyType, ItemType>& rhs) const; // TODO implement these overloads in SearchKey?
+};
+
+#include "tree_entry.h" // todo correct?
+#endif //SOURCE_TREE_ENTRY_H
